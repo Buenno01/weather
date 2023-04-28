4fc73f400bcc7caa37a121bfa66eeb08c2373921
Vinícius Bueno Costa <120047535+Buenno01@users.noreply.github.com>
2023-04-28 13:04:36 -0300

    ..

--- app/connectApi.js
+++ app/connectApi.js
@@ -1,5 +1,5 @@
 async function weatherData (city) {
-    const forecast = await fetch(`http://api.weatherapi.com/v1/current.json?key=b170298837194937afb134328231904&q=${city}&aqi=no`);
+    const forecast = await fetch(`https://api.weatherapi.com/v1/current.json?key=b170298837194937afb134328231904&q=${city}&aqi=no`);
     const convertedForecast = await forecast.json();
     return convertedForecast
 }
